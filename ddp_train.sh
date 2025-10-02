425b46198279845989372bf3d57b79c245aeb18f
XiangGD <67092445+XiangGD@users.noreply.github.com>
2025-10-02 20:21:37 +0800

    Update ddp_train.sh

--- ddp_train.sh
+++ ddp_train.sh
@@ -1,1 +1,1 @@
-python -m torch.distributed.run --nproc_per_node=3 --master_port='12345' ./DFFormer/training_scripts/train.py
+python -m torch.distributed.run --nproc_per_node=3 --master_port='12345' ./dfformer/training_scripts/train.py
